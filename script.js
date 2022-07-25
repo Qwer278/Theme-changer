52a9403bec3a8a2402a1fc3a6b430c9b90c2b5f0
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-07-25 18:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    document.getElementById("text").focus();
+}
+function black(){
+    document.getElementById("one").style.backgroundColor="black";
+    document.getElementById("text").style.backgroundColor="black";
+    document.getElementById("text").style.color="white";
+    document.getElementById("text").style.borderColor ="white";
+    document.getElementById("text").focus();
+};
+function green(){
+    document.getElementById("one").style.backgroundColor="green";
+    document.getElementById("text").style.backgroundColor="green";
+    document.getElementById("text").style.color="black";
+    document.getElementById("text").style.borderColor ="black";
+    document.getElementById("text").focus();
+};
+function yellow(){
+    document.getElementById("one").style.backgroundColor="yellow";
+    document.getElementById("text").style.backgroundColor="yellow";
+    document.getElementById("text").style.color="black";
+    document.getElementById("text").style.borderColor ="black";
+    document.getElementById("text").focus();
+};
+function blue(){
+    document.getElementById("one").style.backgroundColor="blue";
+    document.getElementById("text").style.backgroundColor="blue";
+    document.getElementById("text").style.color="white";
+    document.getElementById("text").style.borderColor ="white";
+    document.getElementById("text").focus();
+};
